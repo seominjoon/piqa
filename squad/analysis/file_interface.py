c2b5651ef1473ed9465dee25bc31284b99601198
Jinhyuk Lee <jinhyuk_lee@korea.ac.kr>
2018-11-23 14:28:42 +0900

    ready for open

--- squad/analysis/file_interface.py
+++ squad/analysis/file_interface.py
@@ -3,7 +3,21 @@ import json
 
 
 class FileInterface(dev.FileInterface):
+    def __init__(self, **kwargs):
+        self.analysis = kwargs['analysis']
+        super(FileInterface, self).__init__(**kwargs)
+
     def load_test(self):
         with open(self._test_path, 'r') as fp:
             squad = json.load(fp)
-        return squad
+
+        if self.analysis == 'eval':
+            return squad
+        elif self.analysis == 'open':
+            test_examples = [{"idx": int("{}".format(idx)),
+                              "cid": "Open_{}".format(idx),
+                              "context": context} for (idx, context) in
+                              enumerate(squad)]
+            return test_examples
+        else:
+            raise ValueError()
