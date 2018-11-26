57b44c881af95b6a7cc6c096d2777d311162e0f4
Jinhyuk Lee <jinhyuk_lee@korea.ac.kr>
2018-11-26 12:41:22 +0900

    open merge edited, tfidf added

--- squad/open_merge.py
+++ squad/open_merge.py
@@ -1,129 +1,105 @@
 import json
 import os
-import argparse
-import dev
-import analysis
-import torch
 import importlib
 import sys
 import numpy as np
-import faiss
 
 from pprint import pprint
-from torch.utils.data import DataLoader
 
 
 # Predefined paths
 PRED_PATH = './open_pred.json'
 
 
-# Customized argparser
-class ArgumentParser(analysis.ArgumentParser):
-    def __init__(self, description='baseline', **kwargs):
-        super(ArgumentParser, self).__init__(description=description)
-
-    def add_arguments(self):
-        super().add_arguments()
-
-    def parse_args(self, **kwargs):
-        args = super().parse_args()
-        args.pred_path = PRED_PATH
-        return args
-
-
 if __name__ == '__main__':
     from_ = importlib.import_module(sys.argv[1])
     FileInterface = from_.FileInterface
-    Processor = from_.Processor
-    Sampler = from_.Sampler
-    Model = from_.Model
-    Loss = from_.Loss
+    ArgumentParser = from_.ArgumentParser
 
     # Parse arguments
     parser = ArgumentParser(description='Open setting evaluation')
     parser.add_arguments()
     args = parser.parse_args()
+    args.pred_path = PRED_PATH
 
     ##### Copied from seve_demo (main.py) #####
     # Load model / processor / interface
-    device = torch.device('cuda' if args.cuda else 'cpu')
     pprint(args.__dict__)
     interface = FileInterface(**args.__dict__)
 
     # Build index
-    with torch.no_grad():
-        phrases = []
-        paras = []
-        results = []
-        embs = []
-        idxs = []
-        iterator = interface.context_load(metadata=True, emb_type=args.emb_type)
-        for _, (cur_phrases, each_emb, metadata) in zip(range(args.num_train_mats), iterator):
-            embs.append(each_emb)
+    phrases = []
+    paras = []
+    results = []
+    embs = []
+    idxs = []
+    iterator = interface.context_load(metadata=True, emb_type=args.emb_type)
+    for _, (cur_phrases, each_emb, metadata) in zip(range(args.num_train_mats), iterator):
+        embs.append(each_emb)
+        phrases.extend(cur_phrases)
+        for span in metadata['answer_spans']:
+            results.append([len(paras), span[0], span[1]])
+            idxs.append(len(idxs))
+        paras.append(metadata['context'])
+    if args.emb_type == 'dense':
+        import faiss
+        emb = np.concatenate(embs, 0)
+
+        d = 4 * args.hidden_size * args.num_heads
+        if args.metric == 'ip' or args.metric == 'cosine':
+            quantizer = faiss.IndexFlatIP(d)  # Exact Search
+        elif args.metric == 'l2':
+            quantizer = faiss.IndexFlatL2(d)
+        else:
+            raise ValueError()
+
+        if args.nlist != args.nprobe:
+            # Approximate Search. nlist > nprobe makes it faster and less accurate
+            if args.bpv is None:
+                if args.metric == 'ip':
+                    search_index = faiss.IndexIVFFlat(quantizer, d, args.nlist, faiss.METRIC_INNER_PRODUCT)
+                elif args.metric == 'l2':
+                    search_index = faiss.IndexIVFFlat(quantizer, d, args.nlist)
+                else:
+                    raise ValueError()
+            else:
+                assert args.metric == 'l2'  # only l2 is supported for product quantization
+                search_index = faiss.IndexIVFPQ(quantizer, d, args.nlist, args.bpv, 8)
+            search_index.train(emb)
+        else:
+            search_index = quantizer
+
+        search_index.add(emb)
+        for cur_phrases, each_emb, metadata in iterator:
             phrases.extend(cur_phrases)
             for span in metadata['answer_spans']:
                 results.append([len(paras), span[0], span[1]])
-                idxs.append(len(idxs))
             paras.append(metadata['context'])
-        if args.emb_type == 'dense':
-            import faiss
-            emb = np.concatenate(embs, 0)
+            search_index.add(each_emb)
 
-            d = 4 * args.hidden_size * args.num_heads
-            if args.metric == 'ip' or args.metric == 'cosine':
-                quantizer = faiss.IndexFlatIP(d)  # Exact Search
-            elif args.metric == 'l2':
-                quantizer = faiss.IndexFlatL2(d)
-            else:
-                raise ValueError()
+        if args.nlist != args.nprobe:
+            search_index.nprobe = args.nprobe
 
-            if args.nlist != args.nprobe:
-                # Approximate Search. nlist > nprobe makes it faster and less accurate
-                if args.bpv is None:
-                    if args.metric == 'ip':
-                        search_index = faiss.IndexIVFFlat(quantizer, d, args.nlist, faiss.METRIC_INNER_PRODUCT)
-                    elif args.metric == 'l2':
-                        search_index = faiss.IndexIVFFlat(quantizer, d, args.nlist)
-                    else:
-                        raise ValueError()
-                else:
-                    assert args.metric == 'l2'  # only l2 is supported for product quantization
-                    search_index = faiss.IndexIVFPQ(quantizer, d, args.nlist, args.bpv, 8)
-                search_index.train(emb)
-            else:
-                search_index = quantizer
+        def search(emb, k):
+            D, I = search_index.search(emb, k)
+            return D, I
+    else:
+        raise NotImplementedError()
 
-            search_index.add(emb)
-            for cur_phrases, each_emb, metadata in iterator:
-                phrases.extend(cur_phrases)
-                for span in metadata['answer_spans']:
-                    results.append([len(paras), span[0], span[1]])
-                paras.append(metadata['context'])
-                search_index.add(each_emb)
+    def retrieve(q_embs, k):
+        D, I = search(q_embs, k)
+        outs = [[(paras[results[i][0]], results[i][1], 
+                       results[i][2], '%.4r' % d.item(),)
+               for d, i in zip(D_k, I_k)] for D_k, I_k in zip(D, I)]
+        return outs
 
-            if args.nlist != args.nprobe:
-                search_index.nprobe = args.nprobe
-
-            def search(emb, k):
-                D, I = search_index.search(emb, k)
-                return D, I
-        else:
-            raise NotImplementedError()
-
-        def retrieve(q_embs, k):
-            D, I = search(q_embs, k)
-            outs = [[(paras[results[i][0]], results[i][1], 
-                           results[i][2], '%.4r' % d.item(),)
-                   for d, i in zip(D_k, I_k)] for D_k, I_k in zip(D, I)]
-            return outs
-
-        if args.mem_info:
-            import psutil
-            import os
-            pid = os.getpid()
-            py = psutil.Process(pid)
-            info = py.memory_info()[0] / 2. ** 30
-            print('Memory Use: %.2f GB' % info)
+    if args.mem_info:
+        import psutil
+        import os
+        pid = os.getpid()
+        py = psutil.Process(pid)
+        info = py.memory_info()[0] / 2. ** 30
+        print('Memory Use: %.2f GB' % info)
 
     print('Index loaded: {}'.format(search_index.ntotal))
         
@@ -142,4 +118,4 @@ if __name__ == '__main__':
     with open(args.pred_path, 'w') as fp:
         json.dump(predictions, fp)
 
-    print('Prediction saved as {}'.format(args.pred_path))
+    print('prediction saved as {}'.format(args.pred_path))
---
+++ squad/tfidf_add.py
@@ -0,0 +1,172 @@
+"""Official merge script for PI-SQuAD v0.1"""
+from __future__ import print_function
+
+import os
+import argparse
+import json
+import sys
+import shutil
+
+import scipy.sparse
+import scipy.sparse.linalg
+import numpy as np
+import numpy.linalg
+
+from scipy.sparse import csr_matrix, hstack, vstack, save_npz
+
+
+# Copied from ../baseline/file_interface
+def _load_squad(squad_path, draft=False):
+    with open(squad_path, 'r') as fp:
+        squad = json.load(fp)
+        examples = []
+        for article in squad['data']:
+            for para_idx, paragraph in enumerate(article['paragraphs']):
+                cid = '%s_%d' % (article['title'], para_idx)
+                if 'context' in paragraph:
+                    context = paragraph['context']
+                    context_example = {'cid': cid, 'context': context}
+                else:
+                    context_example = {}
+
+                if 'qas' in paragraph:
+                    for question_idx, qa in enumerate(paragraph['qas']):
+                        id_ = qa['id']
+                        qid = '%s_%d' % (cid, question_idx)
+                        question = qa['question']
+                        question_example = {'id': id_, 'qid': qid, 'question': question}
+                        if 'answers' in qa:
+                            answers, answer_starts, answer_ends = [], [], []
+                            for answer in qa['answers']:
+                                answer_start = answer['answer_start']
+                                answer_end = answer_start + len(answer['text'])
+                                answers.append(answer['text'])
+                                answer_starts.append(answer_start)
+                                answer_ends.append(answer_end)
+                            answer_example = {'answers': answers, 'answer_starts': answer_starts,
+                                              'answer_ends': answer_ends}
+                            question_example.update(answer_example)
+
+                        example = {'idx': len(examples)}
+                        example.update(context_example)
+                        example.update(question_example)
+                        examples.append(example)
+                        if draft and len(examples) == 100:
+                            return examples
+                else:
+                    example = {'idx': len(examples)}
+                    example.update(context_example)
+                    examples.append(example)
+                    if draft and len(examples) == 100:
+                        return examples
+        return examples
+
+
+# Append document tfidf vector to each paragraphs
+def append_tfidf(context_emb_dir, question_emb_dir, progress, **kwargs):
+    context_emb_dir, context_emb_ext = os.path.splitext(context_emb_dir)
+    question_emb_dir, question_emb_ext = os.path.splitext(question_emb_dir)
+    if context_emb_ext == '.zip':
+        print('Extracting %s to %s' % (context_emb_path, context_emb_dir))
+        shutil.unpack_archive(context_emb_path, context_emb_dir)
+    if question_emb_ext == '.zip':
+        print('Extracting %s to %s' % (question_emb_path, question_emb_dir))
+        shutil.unpack_archive(question_emb_path, question_emb_dir)
+
+    if progress:
+        from tqdm import tqdm
+    else:
+        tqdm = lambda x: x
+
+    context_paths = tuple(os.path.join(context_emb_dir, path)
+                          for path in os.listdir(context_emb_dir)
+                          if os.path.splitext(path)[1] == '.npz' and \
+                          'tfidf' not in path)
+    question_paths = tuple(os.path.join(question_emb_dir, path)
+                          for path in os.listdir(question_emb_dir)
+                          if os.path.splitext(path)[1] == '.npz' and \
+                          'tfidf' not in path)
+    print('Number of contexts to process: {}'.format(len(context_paths)))
+    print('Number of questions to process: {}'.format(len(question_paths)))
+
+    # Load retriever
+    from drqa import retriever
+    ranker = retriever.get_class('tfidf')(tfidf_path=kwargs['retriever_path'],
+                                          strict=False)
+    print('Retriever loaded from {}'.format(args.retriever_path))
+
+    # Read SQuAD to construct squad-docs, squad-ques
+    squad = _load_squad(args.squad_path)
+    assert len(squad) > 0
+    squad_docs = {}
+    squad_ques = {}
+    for item in squad:
+        title = '_'.join(item['cid'].split('_')[:-1])
+        if title not in squad_docs:
+            squad_docs[title] = list()
+        if item['context'] not in squad_docs[title]:
+            squad_docs[title].append(item['context'])
+        squad_ques[item['id']] = item['question']
+
+    # Process contexts first
+    for c_emb_path in tqdm(context_paths):
+        break
+
+        # Dense vector supported only 
+        assert os.path.exists(c_emb_path)
+        par_title = os.path.splitext(os.path.basename(c_emb_path))[0]
+        doc_title = '_'.join(par_title.split('_')[:-1])
+        assert doc_title in squad_docs.keys()
+        c_emb = np.load(c_emb_path)  # shape = [N, d]
+        c_emb = c_emb['arr_0']
+
+        # Get tf-idf vector of the document, then concat
+        tfidf_emb = ranker.text2spvec(' '.join(squad_docs[doc_title]))
+        c_emb = csr_matrix(c_emb)
+        concat_emb = vstack([hstack([emb, tfidf_emb]) for emb in c_emb])
+
+        # Save concatenated vector
+        concat_path = os.path.join(context_emb_dir, par_title + '_tfidf.npz')
+        save_npz(concat_path, concat_emb)
+
+    # Process questions
+    for q_emb_path in tqdm(question_paths):
+        
+        # Dense vector supported only
+        assert os.path.exists(q_emb_path)
+        q_id = os.path.splitext(os.path.basename(q_emb_path))[0]
+        assert q_id in squad_ques
+        q_emb = np.load(q_emb_path)  # shape = [N, d]
+        q_emb = q_emb['arr_0']
+
+        # Get tf-idf vector of question, then concat
+        tfidf_emb = ranker.text2spvec(squad_ques[q_id])
+        q_emb = csr_matrix(q_emb)
+        concat_emb = vstack([hstack([emb, tfidf_emb]) for emb in q_emb])
+
+        # Save concatenated vector
+        concat_path = os.path.join(question_emb_dir, q_id + '_tfidf.npz')
+        save_npz(concat_path, concat_emb)
+
+
+# Predefined paths
+home = os.path.expanduser('~')
+RET_PATH = os.path.join(home, 'Desktop/Jinhyuk/github/DrQA/data', 
+    'wikipedia/docs-tfidf-ngram=2-hash=16777216-tokenizer=simple.npz')
+SQUAD_PATH = os.path.join(home, 'data/squad',
+    'dev-v1.1.json')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='script for appending tf-idf')
+    parser.add_argument('context_emb_dir', help='Context embedding directory')
+    parser.add_argument('question_emb_dir', help='Question embedding directory')
+    parser.add_argument('--retriever-path', type=str, default=RET_PATH,
+                        help='Document Retriever path')
+    parser.add_argument('--squad-path', type=str, default=SQUAD_PATH,
+                        help='SQuAD dataset path')
+    parser.add_argument('--progress', default=False, action='store_true')
+    args = parser.parse_args()
+
+    append_tfidf(**args.__dict__)
+
