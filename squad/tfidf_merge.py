8d322a83b44b5cc8b020190102f3a3b23dd7ad3e
Jinhyuk Lee <jinhyuk_lee@korea.ac.kr>
2018-12-14 22:21:43 +0900

    two

--- squad/tfidf_merge.py
+++ squad/tfidf_merge.py
@@ -141,28 +141,37 @@ if __name__ == '__main__':
     # delete following lines for nsml-free implementation
     import nsml
     if nsml.IS_ON_NSML:
-        def p_load_fn(filename, **kwargs):
-            print('p load embed in', filename)
-            args.p_emb_dir = filename
-        nsml.bind(load=p_load_fn)
-        p_load_path = '%s_embed_%s' % (
-            args.iteration,
-            os.path.splitext(os.path.basename(args.context_path))[0].replace(
-                '.', '_'
-            )
-        )
-        nsml.load(p_load_path, session=args.embed_session)
-
         def q_load_fn(filename, **kwargs):
             print('q load embed in', filename)
             args.q_emb_dir = filename
+
+            def p_load_fn(filename, **kwargs):
+                print('p load embed in', filename)
+                args.p_emb_dir = filename
+
+                # Merge using tfidf
+                predictions = merge_tfidf(**args.__dict__)
+                with open(args.pred_path, 'w') as fp:
+                    json.dump(predictions, fp)
+
+            nsml.bind(load=p_load_fn)
+            p_load_path = '%s_embed_%s' % (
+                args.iteration,
+                os.path.splitext(os.path.basename(args.context_path))[0].replace(
+                    '.', '_'
+                )
+            )
+            nsml.load(p_load_path, session=args.embed_session)
+
         nsml.bind(load=q_load_fn)
         q_load_path = '%s_embed_dev-v1_1-question' % (
             args.iteration
         )
         nsml.load(q_load_path, session=args.embed_session)
 
-    # Merge using tfidf
-    predictions = merge_tfidf(**args.__dict__)
-    with open(args.pred_path, 'w') as fp:
-        json.dump(predictions, fp)
+
+    else:
+        # Merge using tfidf
+        predictions = merge_tfidf(**args.__dict__)
+        with open(args.pred_path, 'w') as fp:
+            json.dump(predictions, fp)
