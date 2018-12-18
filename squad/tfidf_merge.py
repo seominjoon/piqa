fd5861acb5a52a057b78279559042877c33c520d
Jinhyuk Lee <jinhyuk_lee@korea.ac.kr>
2018-12-18 14:07:24 +0900

    space

--- squad/tfidf_merge.py
+++ squad/tfidf_merge.py
@@ -192,7 +192,6 @@ if __name__ == '__main__':
             if os.path.isfile(file_path):
                 os.remove(file_path)
 
-
     else:
         raise NotImplementedError
         # Merge using tfidf
