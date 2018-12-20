6f3ba8934d6003a40eee75157387461bc6a5fe21
Jinhyuk Lee <jinhyuk_lee@korea.ac.kr>
2018-12-20 17:52:50 +0900

    commits

--- squad/run_agg.sh
+++ squad/run_agg.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
 # Download files into preds
-TEST_PATH=preds/tfidf_weighting/baseline/0e+0
-SESS=(96) 
+# TEST_PATH=preds/tfidf_weighting/baseline/3e+1
+# TEST_PATH=preds/tfidf_weighting/bert/3e+1
+# TEST_PATH=preds/tfidf_weighting/bert_large/3e+4
+# TEST_PATH=preds/top_n_docs/baseline/10
+TEST_PATH=preds/top_n_docs/bert/10
+# TEST_PATH=preds/top_n_docs/bert_large/3
 
+mkdir -p $TEST_PATH
+rm -rf $TEST_PATH/*
 cd $TEST_PATH
-for i in ${SESS[@]}
+
+SESS=(1128 1180 1130 1131 1132) 
+# for i in ${SESS[@]}
+for i in $(seq 1226 1230)
 do
   nsml download -s /app/preds/agg_pred.json piqateam/squad_piqa_nfs/$i ./
 done
